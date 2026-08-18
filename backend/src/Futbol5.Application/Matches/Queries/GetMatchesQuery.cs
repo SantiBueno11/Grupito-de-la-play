31462d26d6dff4115ae1c9c36c33cee6d98c449b
Santiago Bueno <sbueno937@gmail.com>
2026-08-18 17:58:54 -0300

    Arreglar rutas de matches y agregar GetMatchesQuery

---
+++ backend/src/Futbol5.Application/Matches/Queries/GetMatchesQuery.cs
@@ -0,0 +1,37 @@
+using Futbol5.Application.Common.DTOs;
+using Futbol5.Application.Common.Interfaces;
+using Futbol5.Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Futbol5.Application.Matches.Queries;
+
+public record GetMatchesQuery : IRequest<List<MatchDto>>;
+
+public class GetMatchesQueryHandler(IApplicationDbContext context)
+    : IRequestHandler<GetMatchesQuery, List<MatchDto>>
+{
+    public async Task<List<MatchDto>> Handle(GetMatchesQuery request, CancellationToken cancellationToken)
+    {
+        var matches = await context.Matches
+            .Include(m => m.MatchPlayers)
+            .ThenInclude(mp => mp.Player)
+            .OrderByDescending(m => m.Date)
+            .ToListAsync(cancellationToken);
+
+        return matches.Select(m => new MatchDto(
+            m.Id,
+            m.Date,
+            m.TeamAName,
+            m.TeamBName,
+            m.ScoreA,
+            m.ScoreB,
+            m.MatchPlayers.Where(mp => mp.Team == Team.A)
+                .Select(mp => new MatchPlayerDto(mp.PlayerId, mp.Player!.Name, mp.Player!.PhotoUrl, mp.HasSpecialTag))
+                .ToList(),
+            m.MatchPlayers.Where(mp => mp.Team == Team.B)
+                .Select(mp => new MatchPlayerDto(mp.PlayerId, mp.Player!.Name, mp.Player!.PhotoUrl, mp.HasSpecialTag))
+                .ToList()
+        )).ToList();
+    }
+}
