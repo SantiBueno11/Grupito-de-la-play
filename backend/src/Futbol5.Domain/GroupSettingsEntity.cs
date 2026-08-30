10e8de7a8ab5f2421ca3444deeeb98314ae6bdbd
SantiBueno11 <sbueno937@gmail.com>
2026-08-29 21:01:42 -0300

    Creada la entidad GroupSettingsEntity

---
+++ backend/src/Futbol5.Domain/GroupSettingsEntity.cs
@@ -0,0 +1,9 @@
+namespace Futbol5.Domain.Entities;
+
+public class GroupSettingsEntity
+{
+    public int Id { get; set; } = 1;
+    public string Name { get; set; } = "Grupito de la play";
+    public string Description { get; set; } = "";
+    public string PhotoUrl { get; set; } = "";
+}
